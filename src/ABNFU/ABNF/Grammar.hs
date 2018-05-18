07b30b0cf32337c256af2e0cc24bf2a9278aab08
Jonathan Merritt <j.s.merritt@gmail.com>
2018-05-18 12:20:33 +1000

    ABNF grammar ("as written")

---
+++ src/ABNFU/ABNF/Grammar.hs
@@ -0,0 +1,133 @@
+{-|
+Module      : ABNFU.ABNF.Grammar
+Description : Haskell data types to describe an ABNF grammar.
+
+The types in this module describe the components of an ABNF grammar
+"as written", corresponding to RFC-5234 and RFC-7405.
+-}
+module ABNFU.ABNF.Grammar
+    ( -- * Types
+      ABNFGrammar(..)
+    , Line(..)
+    , Comment(..)
+    , Rule(..)
+    , Elem(..)
+    , RuleName(..)
+    , LiteralChar(..)
+    , Base(..)
+    , LiteralString(..)
+    , CaseSensitivity(..)
+    , Repeats(..)
+    ) where
+
+import           Data.CaseInsensitive (CI)
+import           Data.Text            (Text)
+
+-- | Parsed contents of an ABNF grammar.
+newtype ABNFGrammar = ABNFGrammar [Line]
+
+-- | Line in an ABNF file.
+data Line
+    -- | Blank line.
+    = LineBlank
+    -- | Line containing a rule followed by an optional comment.
+    | LineRule !Rule !(Maybe Comment)
+    -- | Line containing only a comment.
+    | LineComment !Comment
+
+-- | Comment (RFC-5234 3.9).
+--
+--   > ; this is a comment
+newtype Comment = Comment Text
+
+-- | ABNF rule.
+data Rule
+    -- | Base rule (RFC-5234 2.2).
+    --
+    --   > name = elements crlf
+    = RuleBase !RuleName !Elem
+    -- | Incremental alternatives for a rule (RFC-5234 3.3).
+    --
+    --   > oldrule =/ additional-alternatives
+    | RuleIncrementalAlternative !RuleName !Elem
+
+-- | Expression language of rule elements.
+data Elem
+    -- | Uses a named rule.
+    --
+    --   > name1 = name2
+    = ElemNamedRule !RuleName
+    -- | A character literal (RFC-5234 2.3).
+    --
+    --   > %x61 ; a
+    | ElemChar !LiteralChar
+    -- | A string literal (RFC-5234 2.3 and RFC-7405).
+    --
+    --   > "command string"    ; case-insensitive string (RFC-5234)
+    --   > %i"command string"  ; case-insensitive string (RFC-7405)
+    --   > %s"command string"  ; case-sensitive string (RFC-7405)
+    | ElemString !LiteralString
+    -- | Concatenation of elements (RFC-5234 3.1).
+    --
+    --   > rule = a b c
+    | ElemConcat !Elem !Elem
+    -- | Alternative elements (RFC-5234 3.2).
+    --
+    --   > rule = a / b
+    | ElemAlternative !Elem !Elem
+    -- | Repeated element (RFC-5234 3.6, 3.7 and 3.8).
+    --
+    --   > 5e     ; repeats element e 5 times
+    --   > 2*5e   ; repeats e a minimum of 2 and maximum of 5 times
+    --   > *5e    ; repeats e from zero to 5 times
+    --   > [e]    ; e is optional
+    | ElemRepeat Repeats !Elem
+    -- | Parenthesized elements (RFC-5234 3.5).
+    --
+    --   > (a b)
+    | ElemParen !Elem
+
+-- | Name of a rule (RFC-5234 2.1).
+newtype RuleName = RuleName (CI Text)
+
+-- | Numeric representation of a single terminal character (RFC-5234 2.3).
+data LiteralChar = LiteralChar !Base !Integer
+
+-- | Base in which a numeric character is expressed (RFC-5234 2.3).
+data Base
+    = Binary
+    | Decimal
+    | Hexadecimal
+
+-- | Literal string (RFC-5234 2.3).
+data LiteralString = LiteralString !(Maybe CaseSensitivity) !Text
+
+-- | Case sensitivity of a literal string (RFC-7405).
+data CaseSensitivity
+    = CaseSensitive
+    | CaseInsensitive
+
+-- | Specifies how repeats of an element occur (RFC-5234 3.6, 3.7 and 3.8).
+--
+--   This covers both @<a>*<b>element@ notation and optional @[a]@ notation.
+data Repeats
+    -- | Specifies exactly how many times an element should repeat.
+    --
+    --   > 5e
+    = RepeatsExactly !Integer
+    -- | Specifies the minimum number of times an element should repeat.
+    --
+    --   > 3*e
+    | RepeatsAtLeast !Integer
+    -- | Specifies the maximum number of times an element should repeat.
+    --
+    --   > *5e
+    | RepeatsAtMost !Integer
+    -- | Specifies a range of times an element should repeat.
+    --
+    --   > 3*5e
+    | RepeatsBetween !Integer !Integer
+    -- | Specifies that an element is optional.
+    --
+    --   > [e]
+    | RepeatsOptional
